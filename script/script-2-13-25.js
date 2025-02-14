f5f92065a94e9f1a6915f914a1f9b06cc91718ec
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-02-14 10:57:11 +0200

    form

--- script/script-2-13-25.js
+++ script/script-2-13-25.js
@@ -1,56 +1,56 @@
-// Using key-events
-const myInput = document.querySelector('.keyboard-event input')
-const myp = document.querySelector('.keyboard-event p')
-const mybtn = document.querySelector('.keyboard-event button')
-myInput.addEventListener('keydown', (event) => {
-  if (event.key === 'CapsLock') {
-    console.log(`key=${event.key},code=${event.code}`);
-  }
-})
-  myInput.addEventListener('input', () => {
-    myInput.value = myInput.value.toUpperCase();
-  })
-  window.onscroll = function() {scrollFunction()};
+// // Using key-events
+// const myInput = document.querySelector('.keyboard-event input')
+// const myp = document.querySelector('.keyboard-event p')
+// const mybtn = document.querySelector('.keyboard-event button')
+// myInput.addEventListener('keydown', (event) => {
+//   if (event.key === 'CapsLock') {
+//     console.log(`key=${event.key},code=${event.code}`);
+//   }
+// })
+//   myInput.addEventListener('input', () => {
+//     myInput.value = myInput.value.toUpperCase();
+//   })
+//   window.onscroll = function() {scrollFunction()};
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybtn.style.color = "red";
-    } else {
-      mybtn.style.color = "black";
-    }
-  }
+//   function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//       mybtn.style.color = "red";
+//     } else {
+//       mybtn.style.color = "black";
+//     }
+//   }
 
-  myInput.addEventListener('focus', ()=>{
-    myInput.style.backgroundColor = 'red'
-  })
+//   myInput.addEventListener('focus', ()=>{
+//     myInput.style.backgroundColor = 'red'
+//   })
 
-  myInput.addEventListener('blur', ()=>{
-    myInput.style.backgroundColor = ''
-  })
+//   myInput.addEventListener('blur', ()=>{
+//     myInput.style.backgroundColor = ''
+//   })
 
-  window.addEventListener('hashchange', ()=>{
-    console.log(`changed ${location.hash}`)
-    if(location.hash === 'home'){
-      console.log('home')
-    } else{
-      console.log('about')
-    }
-  })
+//   window.addEventListener('hashchange', ()=>{
+//     console.log(`changed ${location.hash}`)
+//     if(location.hash === 'home'){
+//       console.log('home')
+//     } else{
+//       console.log('about')
+//     }
+//   })
 
-  const menu = document.getElementById('menu');
-  menu.addEventListener('click', (e)=>{
-    let target = e.target;
-    switch(target.id){
-      case 'home':
-        console.log('Home clicked')
-        break;
-        case 'dashboard':
-          console.log('Dashboard clicked')
-          break;
-        case 'report':
-          console.log('Report has clicked')
-          break;
-        default:
-          console.log('Unknown click')
-    }
-  })
+//   const menu = document.getElementById('menu');
+//   menu.addEventListener('click', (e)=>{
+//     let target = e.target;
+//     switch(target.id){
+//       case 'home':
+//         console.log('Home clicked')
+//         break;
+//         case 'dashboard':
+//           console.log('Dashboard clicked')
+//           break;
+//         case 'report':
+//           console.log('Report has clicked')
+//           break;
+//         default:
+//           console.log('Unknown click')
+//     }
+//   })
---
+++ script/script-2-14-25.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('signup')
+// Get form element by index
+console.log(form.elements[0])
+// Get form element by name here you can also use this syntx when you want to use id
+console.log(form.elements['email'])
+
+const name = form.elements['name']
+const email = form.elements['email']
+
+
+
