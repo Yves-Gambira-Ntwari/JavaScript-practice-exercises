9c1599cfbabd7e21be8ffb3fc124af135f2bd834
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-03-04 17:28:05 +0200

    class

--- mentalsession/mental.js
+++ mentalsession/mental.js
@@ -1,11 +1,32 @@
-function removeRep(arr){
-  return arr.filter((item, index) => arr.indexOf(item) == index)
-  // let unique = []
-  // arr.forEach(i => {
-  //   if(!unique.includes(i)){
-  //     unique.push(i)
-  //   }
-  // })
-  // console.log(unique)
+// function removeRep(arr){
+//   return arr.filter((item, index) => arr.indexOf(item) == index)
+//   // let unique = []
+//   // arr.forEach(i => {
+//   //   if(!unique.includes(i)){
+//   //     unique.push(i)
+//   //   }
+//   // })
+//   // console.log(unique)
+// }
+// console.log(removeRep([1,'d',2,'d']))
+
+// Create a class Product with properties name (string), 
+// price (number), and methods: getDetails(): Returns a string representation of the product details (name and price). 
+// applyDiscount(discount): Applies a discount (percentage) to the product's price and returns the discounted price.
+
+class Product{
+  constructor(proName, price, discount){
+    this.proName = proName;
+    this.price = price;
+    this.discount =discount;
+  }
+  getDetails(){
+    return `You sell ${this.proName} cost ${this.price}`
+  }
+  applyDiscout(){
+    return `You sell ${this.proName}  discount is ${(this.price * this.discount)/100}%`
+  }
 }
-console.log(removeRep([1,'d',2,'d']))
+const ProductObj = new Product('Ibitoki', 120, 10)
+console.log(ProductObj.getDetails())
+console.log(ProductObj.applyDiscout())
