3e597f24f29043b805456f48553da1fc759fc897
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-04-22 21:16:56 +0200

    Start on node js

---
+++ Nodejs/index.js
@@ -0,0 +1,5 @@
+import { getPosts } from './postController.js'
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utilis')
+// console.log(`Random number to generate: ${generateRandomNumber()}`)
+// console.log(`Clcius to the fahrenheit: ${celciusToFahrenheit(60)}`)
+console.log(getPosts())
