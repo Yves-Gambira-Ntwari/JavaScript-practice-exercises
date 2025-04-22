19d296815caa9a491ee957d35c6fa5b27ada102b
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-04-22 22:46:36 +0200

    Server and handle HTML  files

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,8 +1,34 @@
 import http from 'http';
-const PORT = 8000;
-const server = http.createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
+const server = http.createServer( async(req, res) => {
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename)
+  try {
+    if (req.method == 'GET') {
+      let filePaht;
+      if (req.url === "/") {
+        filePaht = path.join(__dirname, 'public', 'index.html')
+      } else if (req.url === "/about") {
+        filePaht = path.join(__dirname,'public', 'about.html')
+      } else {
+        throw new Error("File Not Found")
+      }
+
+      const data = await fs.readFile(filePaht)
+      res.setHeader('Content-Type', 'text/html')
+      res.write(data)
+      res.end()
+    } else {
+      throw new Error("Method Not Allowed")
+    }
+  } catch (err) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end('Server Error')
+  }
+
 });
 
 server.listen(PORT, () => {
