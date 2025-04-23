cd9c1eaa45c0a892d6f8d05b1c93475e9123e720
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-04-23 20:16:50 +0200

    server and methods

--- Nodejs/server2.js
+++ Nodejs/server2.js
@@ -9,34 +9,71 @@ const users = [
   {id: 40, name: 'Ntwari'}
 ]
 
+const logger = ((res, req, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+const jsonMiddleware = ((res, req, next) =>{
+  res.setHeader('Content-Type', 'application/json')
+  next()
+
+})
+const getUserHandler = ((res, req) =>{
+res.write(JSON.stringify(users))
+res.end()
+})
+
+const getUserByIdHandler = ((res,req) =>{
+  const id = req.url.split('/')[3]
+  const user = users.find((user) => user.id === parseInt(id))
+
+  if(user){
+    res.setHeader('Content-Type', 'application/json')
+  res.write(JSON.stringify(user))
+  res.end()
+  }else{
+    res.setHeader('Content-Type', 'application/json')
+  res.statusCode = 404
+  res.write(JSON.stringify({message: "user not Found"}))
+  res.end()
+  }
+})
+
+const notFoundHandler = ((res, req) =>{
+  res.statusCode = 404
+  res.write(JSON.stringify({message: "not Found"}))
+  res.end()
+})
+
+const createUserHandler = ((res, req) =>{
+  let body = ''
+  req.on('data', (chunk) =>{
+    body += chunk.toString()
+  })
+  req.on('end', ()=>{
+    const newUser = JSON.parse(body)
+    users.push(newUser)
+    res.statusCode = 201
+    res.write(JSON.stringify(newUser))
+    res.end()
+  })
+})
 
 const server = createServer((req, res) =>{
-  if(req.method == 'GET' && req.url == '/api/users'){
-    res.setHeader('Content-Type', 'application/json')
-    res.write(JSON.stringify(users))
-  } 
-  else if(req.method == 'GET' &&  req.url.match(/\/api\/users\/([0-9]+)/)){
-    const id = req.url.split('/')[3]
-    const user = users.find((user) => user.id === parseInt(id))
-    if(user){
-      res.setHeader('Content-Type', 'application/json')
-    res.write(JSON.stringify(user))
-    res.end()
-    }else{
-      res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.write(JSON.stringify({message: "user not Found"}))
-    res.end()
+  logger(res, req, () => {
+    if(req.method == 'GET' && req.url == '/api/users'){
+      getUserHandler(req, res)
+    } 
+    else if(req.method == 'GET' &&  req.url.match(/\/api\/users\/([0-9]+)/)){
+      getUserByIdHandler(res, req)
+    } else if(req.method == 'POST' && req.url == '/api/users'){
+      createUserHandler(res, req)
     }
-    
-  }
-  
-  else{
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.write(JSON.stringify({message: "not Found"}))
-    res.end()
-  }
+    else{
+      notFoundHandler(res, req)
+    }
+  })
 })
 
 server.listen(PORT, () =>{
