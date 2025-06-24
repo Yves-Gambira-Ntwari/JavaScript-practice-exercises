55643be07ea0ee4210bb1255c414530ba606c230
The Gym <The Gym@DESKTOP-1HKCBRB>
2025-06-24 10:58:19 +0200

    get middle letters

---
+++ kata/stringCount.js
@@ -0,0 +1,18 @@
+function getMiddle(s) {
+  const length = s.length
+  let charAt;
+  if(length % 2 === 0){
+    const middlenmber = length / 2
+    const middlenmber2 = (length / 2) + -1
+    charAt = `${s.charAt(middlenmber2)}${s.charAt(middlenmber)}`
+    console.log(middlenmber)
+    console.log(middlenmber2)
+
+  } else{
+     const middlenmber = length / 2
+     charAt = s.charAt(middlenmber)
+  }
+    return charAt;
+}
+
+console.log(getMiddle("middle"))
