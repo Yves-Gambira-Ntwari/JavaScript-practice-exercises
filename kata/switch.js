1274b54b1e787bd3f01ca31941dea4206ccb8956
Gambira <user@LAPTOP-G3JCD04>
2025-07-02 10:42:33 +0200

    switch number to to it's name

--- kata/switch.js
+++ kata/switch.js
@@ -1,34 +1,34 @@
-// function switchItUp(number){
-//   switch(number){
-//     case 0:
-//       return "Zero"
-//       break;
-//     case 1:
-//       return "One"
-//       break
-//     case 2:
-//       return "Two"
-//     case 3:
-//     return "Three"
-//     case 4:
-//       return "Four"
-//     case 5:
-//       return "Five"
-//       break
-//       case 6:
-//         return "Six"
-//         break
-//       case 7:
-//         return "Seven"
-//         break
-//       case 8:
-//         return "Eight"
-//       case 9:
-//         return "Nine"
-//     default:
-//       return "Range 0-9"
-//   }
-// }
-// function switchItUp(number){
-//   return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
-// }
+function switchItUp(number){
+  switch(number){
+    case 0:
+      return "Zero"
+      break;
+    case 1:
+      return "One"
+      break
+    case 2:
+      return "Two"
+    case 3:
+    return "Three"
+    case 4:
+      return "Four"
+    case 5:
+      return "Five"
+      break
+      case 6:
+        return "Six"
+        break
+      case 7:
+        return "Seven"
+        break
+      case 8:
+        return "Eight"
+      case 9:
+        return "Nine"
+    default:
+      return "Range 0-9"
+  }
+}
+function switchItUp(number){
+  return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
+}
