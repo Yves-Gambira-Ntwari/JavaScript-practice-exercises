66f95e707dac1fc8dc2fd26e1bf00d02b0eaa66b
Gambira <user@LAPTOP-G3JCD04>
2025-07-16 12:49:18 +0200

    new

--- kata/findNotEqual.js
+++ kata/findNotEqual.js
@@ -1,14 +1,4 @@
 function findUniq(arr) {
-//  const same =  arr.filter((item,index) => index !== arr.indexOf(item))
-//  let result = 0
-//  for(let item of arr){
-//   for(let newitem of same){
-//     if(item !== newitem){
-//        result = item
-//     }
-//   }
-//  }
-//  return result
 let count ={}
 arr.forEach(item=>{
   count[item] = (count[item] ||0)+1
